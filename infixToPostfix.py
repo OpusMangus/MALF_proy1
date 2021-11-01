bb7da57aae83cec77b3f120e929394001e759b54
Joaquin S <joaq4699@hotmail.com>
2021-10-31 22:51:24 -0300

    Created infixToPostfix.py

---
+++ infixToPostfix.py
@@ -0,0 +1,16 @@
+
+
+def infixToPostfix(regexString):
+    output = []
+    stack = []
+
+    strLen = len(regexString)
+    return strLen
+
+
+
+
+asdf = 'asdf'
+
+num = infixToPostfix(asdf)
+print(num)

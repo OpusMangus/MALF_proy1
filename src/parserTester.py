a2c0ee0b158cd347157e0d72714270e74d36605d
Joaquin S <joaq4699@hotmail.com>
2021-11-02 19:14:34 -0300

    Actualizado modulo de testeo para parser

--- src/parserTester.py
+++ src/parserTester.py
@@ -1,6 +1,13 @@
 from regexParser.parser import parse
 
 
+def preorder(t):
+    if t is not None:
+        print(t.value, end='')
+        preorder(t.left)
+        preorder(t.right)
+
+
 def inorder(t):
     if t is not None:
         inorder(t.left)
@@ -8,7 +15,22 @@ def inorder(t):
         inorder(t.right)
 
 
-regex = '(a.b)*'
+def postorder(t):
+    if t is not None:
+        postorder(t.left)
+        postorder(t.right)
+        print(t.value, end='')
+
+
+regex = '(p*|(a.c*.b))*'
 
 t = parse(regex)
+print('preorder: ', end='')
+preorder(t)
+print()
+print('inorder: ', end='')
 inorder(t)
+print()
+print('postorder: ', end='')
+postorder(t)
+print()

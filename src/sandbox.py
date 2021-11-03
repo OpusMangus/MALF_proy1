93478b5e33b56891ef300e5cfb2dae3435c4bed2
Joaquin S <joaq4699@hotmail.com>
2021-11-03 18:20:35 -0300

    fix construcción edges del sandbox

--- src/sandbox.py
+++ src/sandbox.py
@@ -8,10 +8,10 @@ q0 = NDFA_node('q0')
 q1 = NDFA_node('q1')
 q2 = NDFA_node('q2', True)
 
-edg1 = NDFA_edge('a')
+edg1 = NDFA_edge('a', q1)
 edg1.target = q1
 
-edg2 = NDFA_edge('b')
+edg2 = NDFA_edge('b', q2)
 edg2.target = q2
 
 q0.edges.append(edg1)
@@ -28,59 +28,61 @@ k6 = NDFA_node('q6')
 k7 = NDFA_node('q7', True)
 
 # q0
-ed1 = NDFA_edge('_')
+ed1 = NDFA_edge('_', k1)
 ed1.target = k1
 
-ed2 = NDFA_edge('_')
+ed2 = NDFA_edge('_', k3)
 ed2.target = k3
 
 k0.edges.append(ed1)
 k0.edges.append(ed2)
 
 # q1
-ed3 = NDFA_edge('a')
+ed3 = NDFA_edge('a', k2)
 ed3.target = k2
 
 k1.edges.append(ed3)
 
 # q2
-ed4 = NDFA_edge('_')
+ed4 = NDFA_edge('_', k7)
 ed4.target = k7
 
 k2.edges.append(ed4)
 
 # q3
-ed5 = NDFA_edge('_')
-ed5.target(k4)
+ed5 = NDFA_edge('_', k4)
+ed5.target = k4
 
-ed6 = NDFA_edge('_')
-ed6.target(k6)
+ed6 = NDFA_edge('_', k6)
+ed6.target = k6
 
 k3.edges.append(ed5)
 k3.edges.append(ed6)
 
 # q4
-ed7 = NDFA_edge('b')
-ed7.target(k5)
+ed7 = NDFA_edge('b', k5)
+ed7.target = k5
 
 k4.edges.append(ed7)
 
 # q5
-ed8 = NDFA_edge('_')
+ed8 = NDFA_edge('_', k4)
 ed8.target = k4
 
-ed9 = NDFA_edge('_')
+ed9 = NDFA_edge('_', k6)
 ed9.target = k6
 
 k5.edges.append(ed8)
 k5.edges.append(ed9)
 
 # q6
-ed10 = NDFA_edge('_')
+ed10 = NDFA_edge('_', k7)
 ed10.target = k7
 
 k6.edges.append(ed10)
 
-t1 = automata("asdf")
-t1.get_transition_table()
-print(t1.NDFA)
+# Tests
+nodes = [k3, k4, k6, k3, k3, k1, k4]
+
+for node in set(nodes):
+    print(node.value)

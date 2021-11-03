e5e4354810785abbb7b9dff227f74f3f7c2fd392
Joaquin S <joaq4699@hotmail.com>
2021-11-03 18:10:21 -0300

    Funcion move implementada

--- src/automatas/NDFA/automata.py
+++ src/automatas/NDFA/automata.py
@@ -4,3 +4,11 @@ class automata:
 
     def get_transition_table(self):
         return
+
+    def move(origin_states, char):
+        target_states = []
+        for state in origin_states:
+            for edge in state.edges:
+                if(edge.in_char == char):
+                    target_states.append(edge.target)
+        return set(target_states)

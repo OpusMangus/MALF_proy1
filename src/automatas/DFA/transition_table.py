546077c739f0f9efb97ac17cd6544f4103fe8aa6
Joaquin S <joaq4699@hotmail.com>
2021-11-03 23:23:40 -0300

    poblada clase transition_table

--- src/automatas/DFA/transition_table.py
+++ src/automatas/DFA/transition_table.py
@@ -2,3 +2,35 @@ class transition_table:
     def __init__(self, start_node, alphabet):
         self.NDFA = start_node
         self.alphabet = alphabet
+
+    def get_transition_table(self):
+        return
+
+    def move(self, origin_states, char):
+        target_states = []
+        for state in origin_states:
+            for edge in state.edges:
+                if(edge.in_char == char):
+                    target_states.append(edge.target)
+        return set(target_states)
+
+    def e_closure(self, node_from):
+        #set with every node reachable by epsilon
+        closure = set()
+        #to visit stack
+        reachable = []
+        #for every next state from this node
+        for edge in node_from.edges:
+            if(edge.in_char == '_'):
+                reachable.append(edge.target) #add node to the DFS stack
+                closure.add(edge.target) #add value of the new node to the set
+        #consume the stack until empty
+        while(len(reachable) > 0):
+            reached = reachable.pop() #pop node
+            #for every edge in node check if epsilon
+            for edge in reached.edges:
+                if(edge.in_char == '_'):
+                    if(not (edge.target.value in closure)):
+                        reachable.append(edge.target)
+                        closure.add(edge.target)
+        return closure

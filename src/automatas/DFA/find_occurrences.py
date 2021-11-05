459efd65a232a96c3e2d473f25d8c7cf7f523f25
Joaquin S <joaq4699@hotmail.com>
2021-11-05 20:08:22 -0300

    Find matches con error

---
+++ src/automatas/DFA/find_occurrences.py
@@ -0,0 +1,40 @@
+def find_ocurrences(DFA, string):
+    matches = []
+
+    i = 0
+    while i < len(string):
+        match_end_index = sub_string_max_match(DFA, string[i:])
+        if(match_end_index > -1):
+            matches.append([i, i + match_end_index])
+            i = i + match_end_index + 1
+        else:
+            i += 1
+
+    return matches
+
+def sub_string_max_match(DFA, substring):
+    start_node = DFA.start_node
+    node_stack = []
+    char_stack = list(substring)
+
+    node_stack.append(start_node)
+    max_final_pos = -1
+
+    char_count = 0
+    for char in char_stack: #Se consume un caracter
+        if(len(node_stack) > 0): #Se llega a un nodo nuevo (puede ser el inicial)
+            current_node = node_stack.pop()
+            
+            if current_node.final: #Si es un nodo final, se reemplaza la actual posición máxima de match
+                max_final_pos = char_count
+            for edge in current_node.edges: #Se busca si se puede llegar a otro nodo con el caracter consumido
+                if edge.in_char == char:
+                    node_stack.append(edge.target)
+                    char_count += 1
+                    break
+        else: #Se llega a un estado sin salida
+            print(substring + ' ' + str(max_final_pos))
+            return max_final_pos
+        
+    print(substring + ' ' + str(max_final_pos))
+    return max_final_pos
